8f392db385a8f2ec9d231afcbd3ca1928a4e8014
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-26 17:29:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.size == 1 
-    return array[0]
+    array[0]
   elsif array.size == 2 
-    return array[1]
+    array[1]
   else 
-    return array[0..-2].join(',') + "and" + array[-1]
+    array[0..-2].join(',') + "and" + array[-1]
 end
